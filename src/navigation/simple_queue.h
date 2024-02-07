d46a57b02d06c6e1d36ce3ca1233232d8b8226e5
Sun O'Brien <suncs@utexas.edu>
2024-02-06 21:14:30 -0600

    i fixed the weird queue class

--- src/navigation/simple_queue.h
+++ src/navigation/simple_queue.h
@@ -39,33 +39,34 @@ class SimpleQueue {
   // Insert a new value, with the specified priority. If the value
   // already exists, its priority is updated.
   void Push(const Value& v, const Priority& p) {
-    for (auto& x : values_) {
-      // If the value already exists, update its priority, re-sort the priority
-      // queue, and return.
-      if (x.first == v) {
-        x.second = p;
-        Sort();
-        return;
-      }
-    }
+    // for (auto& x : values_) {
+    //   // If the value already exists, update its priority, re-sort the priority
+    //   // queue, and return.
+    //   if (x.first == v) {
+    //     x.second = p;
+    //     Sort();
+    //     return;
+    //   }
+    // }
     // Find where this value should go, and insert it there.
-    for (size_t i = 0; i < values_.size(); ++i) {
-      if (values_[i].second > p) {
-        values_.insert(values_.begin() + i, make_pair(v, p));
-        return;
-      }
-    }
-    values_.insert(values_.end(), make_pair(v, p));
+    // for (size_t i = 0; i < values_.size(); ++i) {
+    //   if (values_[i].second > p) {
+    //     values_.insert(values_.begin() + i, make_pair(v, p));
+    //     return;
+    //   }
+    // }
+    // values_.insert(values_.end(), make_pair(v, p));
+    values_.push_back(make_pair(v, p));
   }
 
   // Sorts the priorities.
-  void Sort() {
-    static const auto comparator = 
-        [](const pair<Value, Priority>& v1, const pair<Value, Priority>& v2) {
-      return (v1.second < v2.second);
-    };
-    sort(values_.begin(), values_.end(), comparator);
-  }
+  // void Sort() {
+  //   static const auto comparator = 
+  //       [](const pair<Value, Priority>& v1, const pair<Value, Priority>& v2) {
+  //     return (v1.second < v2.second);
+  //   };
+  //   sort(values_.begin(), values_.end(), comparator);
+  // }
 
   // Retreive the value with the highest priority.
   Value Pop() {
@@ -73,9 +74,9 @@ class SimpleQueue {
       fprintf(stderr, "ERROR: Pop() called on an empty queue!\n");
       exit(1);
     }
-    Sort();
-    const Value v = values_.back().first;
-    values_.resize(values_.size() - 1);
+    // Sort();
+    const Value v = values_.front().first;
+    values_.pop_front();
     return v;
   }
 
