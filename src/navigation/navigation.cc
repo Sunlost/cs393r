4326d583cfe5513fa1a5fdee9266047d78f4d8d7
Sun O'Brien <suncs@utexas.edu>
2024-02-06 21:39:57 -0600

    idk lol

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -326,6 +326,8 @@ void Navigation::toc1dstraightline() {
   switch(phase) {
     case PHASE_ACCEL:
       drive_msg_.velocity = 1;
+      toc_queue.Push(0.2, cycle_num);
+      printf("pushed %f to queue with value %ld. now size %d.\n", 0.2, cycle_num, toc_queue.Size());
     break;
 
     case PHASE_CRUISE:
@@ -334,6 +336,8 @@ void Navigation::toc1dstraightline() {
 
     case PHASE_DECEL:
       drive_msg_.velocity = 0;
+      toc_queue.Push(-0.2, cycle_num);
+      printf("pushed %f to queue with value %ld. now size %d.\n", 0.2, cycle_num, toc_queue.Size());
     break;
 
     default:
@@ -343,8 +347,8 @@ void Navigation::toc1dstraightline() {
 
   // 7. save past state
   prev_loc = odom_loc_;
-  toc_queue.Push(v_f - v_i, cycle_num);
-  printf("pushed %f to queue with value %ld. now size %d.\n", v_f - v_i, cycle_num, toc_queue.Size());
+  // toc_queue.Push(v_f - v_i, cycle_num);
+  // printf("pushed %f to queue with value %ld. now size %d.\n", v_f - v_i, cycle_num, toc_queue.Size());
 
   return;
 }
